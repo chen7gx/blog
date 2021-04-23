100a95bd28eeb369a723b694b87219da1b4d83ce
cx <13126625252@163.com>
2021-04-23 10:15:46 +0800

    blog

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,6 +2,7 @@
 	<div>
 		<div class="container">
       	<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="chen7gx.blog.blog" moduleType="Post"  />
 		</div>
 	</div>
 </template>
--- x/blog/keeper/query.go
+++ x/blog/keeper/query.go
@@ -2,8 +2,8 @@ package keeper
 
 import (
 	// this line is used by starport scaffolding # 1
-
 	"github.com/chen7gx/blog/x/blog/types"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -20,6 +20,12 @@ func NewQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
 
 		switch path[0] {
 		// this line is used by starport scaffolding # 2
+		case types.QueryGetPost:
+			return getPost(ctx, path[1], k, legacyQuerierCdc)
+
+		case types.QueryListPost:
+			return listPost(ctx, k, legacyQuerierCdc)
+
 		default:
 			err = sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint: %s", types.ModuleName, path[0])
 		}
